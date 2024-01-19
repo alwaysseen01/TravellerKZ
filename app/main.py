4725bf293d4bb5893a8d2e3d833ae8b91b553574
Igor Ruzhilov <iruzhilov@gmail.com>
2024-01-19 15:17:24 +0600

    project structure: add basic project structure

--- app/main.py
+++ app/main.py
@@ -1,3 +1,5 @@
+import os
+
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -11,3 +13,8 @@ async def root():
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
+
+
+if __name__ == "__main__":
+    # ONLY WHILE DEVELOPING
+    os.system("uvicorn main:app --reload")
